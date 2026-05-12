b39e709af89232b08c86f014d3072bacd16125a9
robert123-python <alejandroanave246@gmail.com>
2026-05-12 08:19:40 -0400

    Actualizacion

--- database/schema.sql
+++
@@ -1,8 +0,0 @@
-/*Tabla usuarios*/
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT,
-  googleId TEXT UNIQUE
-);
--- server/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-export function profile(req, res) {
-
-  res.json({
-    message: "Ruta protegida",
-    user: req.user
-  });
-
-}
